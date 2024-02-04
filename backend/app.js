148a7e321342c82cdd3116f2b5b5b36d2bb9b30f
Priyanshu Gupta <priyanshug712@gmail.com>
2024-02-05 04:09:22 +0530

    add social media links

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const app=express();
+const path=require("path");
+const hbs= require("hbs");
+const port= process.env.PORT || 8000;
+
+
+const static_path=path.join(__dirname, "../frontend");
+const template_path= path.join(__dirname, "../frontend/views");
+const partials_path= path.join(__dirname, "../frontend/partials");
+
+app.use(express.static(static_path));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.set("view engine", "hbs");
+app.set("views", template_path);
+hbs.registerPartials(partials_path);
+
+app.get("/", (req,res)=>{
+    res.render("index")
+});
+
+app.listen(port, () => {
+    console.log(`Server successfully running on port ${port}`);
+  });
---
+++ frontend/js/index.js
@@ -0,0 +1,15 @@
+let prevScrollPos = window.scrollY;
+
+window.onscroll = function() {
+  let currentScrollPos = window.scrollY;
+
+  if (prevScrollPos > currentScrollPos) {
+    // Scroll up
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    // Scroll down
+    document.getElementById("navbar").style.top = "-67px"; // Adjust the value as needed
+  }
+
+  prevScrollPos = currentScrollPos;
+};
