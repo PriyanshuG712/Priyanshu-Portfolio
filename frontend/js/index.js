62d7ed38920deb43a413ebe94ef7135f17c222eb
Priyanshu Gupta <priyanshug712@gmail.com>
2024-02-05 13:25:38 +0530

    added image section

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -13,3 +13,10 @@ window.onscroll = function() {
 
   prevScrollPos = currentScrollPos;
 };
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+var typed= new Typed(".typeWriter",{
+  strings: ["Full Stack Developer", "Java Developer", "Future Senior Dev"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true
+});
