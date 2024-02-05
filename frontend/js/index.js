8e33bbda3a7ed7f8879dc342febb05cb54382009
Priyanshu Gupta <priyanshug712@gmail.com>
2024-02-05 13:54:25 +0530

    navbar animation bug removed

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -13,10 +13,4 @@ window.onscroll = function() {
 
   prevScrollPos = currentScrollPos;
 };
-<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
-var typed= new Typed(".typeWriter",{
-  strings: ["Full Stack Developer", "Java Developer", "Future Senior Dev"],
-  typeSpeed: 150,
-  backSpeed: 150,
-  loop: true
-});
+
